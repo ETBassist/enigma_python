359d545a2e78af5404f19dacfe4f46e281f47bb2
Eugene Theriault <hybridbassist@gmail.com>
2020-11-19 19:00:10 -0800

    Make test for make_cipher method

--- test/key_generator_test.py
+++ test/key_generator_test.py
@@ -8,3 +8,9 @@ def test_it_can_take_date_and_generate_offsets():
 def test_it_can_take_four_digit_number_and_return_key():
     key_gen = KeyGenerator()
     assert key_gen.make_key('02715') == [2, 27, 71, 15]
+
+def test_it_can_combine_keys_and_offsets_into_cipher():
+    key_gen = KeyGenerator()
+    offsets = [1, 0, 2, 5]
+    key = [2, 27, 71, 15]
+    assert key_gen.make_cipher(key, offsets) == [3, 27, 73, 20]
