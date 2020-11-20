eae58b85a4dd96e2a70fee31b6448a37243cc6a2
Eugene Theriault <hybridbassist@gmail.com>
2020-11-20 10:51:04 -0800

    Refactor encrypt/decrypt

--- lib/enigma.py
+++ lib/enigma.py
@@ -10,30 +10,26 @@ class Enigma:
     def encrypt(self, phrase, key, date):
         phrase = phrase.lower()
         cipher = self.key_gen.make_cipher(key, date)
+        encrypted = self.__rotate_phrase(phrase, cipher, 'left')
+        return ''.join(encrypted)
+
+    def decrypt(self, encrypted_phrase, key, date):
+        encrypted_phrase = encrypted_phrase.lower()
+        cipher = self.key_gen.make_cipher(key, date)
+        decrypted = self.__rotate_phrase(encrypted_phrase, cipher, 'right')
+        return ''.join(decrypted)
+
+    def __rotate_phrase(self, phrase, cipher, direction):
         result = []
         for char_index, letter in enumerate(phrase):
             alphabet = self.__create_characters()
             if letter in alphabet:
                 index = alphabet.index(letter)
-                rotated_alpha = self.rotor.left_rotate(alphabet, cipher[char_index % 4])
+                rotated_alpha = self.rotor.rotate(alphabet, cipher[char_index % 4], direction)
                 result.append(rotated_alpha[index])
             else:
                 result.append(letter)
-        return ''.join(result)
-
-    def decrypt(self, encrypted_phrase, key, date):
-        encrypted_phrase = encrypted_phrase.lower()
-        cipher = self.key_gen.make_cipher(key, date)
-        result = []
-        for char_index, letter in enumerate(encrypted_phrase):
-            alphabet = self.__create_characters()
-            if letter in alphabet:
-                index = alphabet.index(letter)
-                rotated_alpha = self.rotor.right_rotate(alphabet, cipher[char_index % 4])
-                result.append(rotated_alpha[index])
-            else:
-                result.append(letter)
-        return ''.join(result)
+        return result
 
     def __create_characters(self):
         alphabet = list(string.ascii_lowercase)
