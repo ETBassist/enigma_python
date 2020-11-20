b7f3a64d7bcf6beea5eb6d794f5069150b9b6fe5
Eugene Theriault <hybridbassist@gmail.com>
2020-11-19 18:33:52 -0800

    Create test for making key

--- test/key_generator_test.py
+++ test/key_generator_test.py
@@ -4,3 +4,7 @@ from lib.key_generator import *
 def test_it_can_take_date_and_generate_offsets():
     key_gen = KeyGenerator()
     assert key_gen.make_offsets('040895') == [1, 0, 2, 5]
+
+def test_it_can_take_four_digit_number_and_return_key():
+    key_gen = KeyGenerator()
+    assert key_gen.make_key('02715') == [2, 27, 71, 15]
