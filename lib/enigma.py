7a37c67f3b95e06c083efc1c2f69e3f14f206088
Eugene Theriault <hybridbassist@gmail.com>
2020-11-19 20:19:55 -0800

    Create encrypt method

--- lib/enigma.py
+++ lib/enigma.py
@@ -1,9 +1,23 @@
+import pdb
 import string
 from rotator import *
+from key_generator import *
 
 class Enigma:
     def __init__(self):
-        rotor = Rotator()
+        self.rotor = Rotator()
+        self.key_gen = KeyGenerator()
 
     def encrypt(self, phrase, key, date):
-        alphabet = list(string.ascii_lowercase)
+        cipher = self.key_gen.make_cipher(key, date)
+        result = []
+        for lindex, letter in enumerate(phrase):
+            alphabet = list(string.ascii_lowercase)
+            alphabet.append(' ')
+            if letter in alphabet:
+                index = alphabet.index(letter)
+                rotated_alpha = self.rotor.rotate(alphabet, cipher[lindex % 4])
+                result.append(rotated_alpha[index])
+            else:
+                result.append(letter)
+        return ''.join(result)
