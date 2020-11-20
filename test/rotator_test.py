149b45489ac68b299bc4291ce66c37cbbdb05ce6
Eugene Theriault <hybridbassist@gmail.com>
2020-11-20 10:35:35 -0800

    Create test for rotate integrated method

--- test/rotator_test.py
+++ test/rotator_test.py
@@ -12,3 +12,11 @@ def test_it_can_right_rotate_an_array():
     original = [1, 2, 3, 4, 5, 6]
     expected = [5, 6, 1, 2, 3, 4]
     assert rotator.right_rotate(original, 2) == expected
+
+def test_it_can_rotate_in_both_directions():
+    rotator = Rotator()
+    original = [1, 2, 3, 4, 5, 6]
+    expected1 = [5, 6, 1, 2, 3, 4]
+    expected2 = [6, 1, 2, 3, 4, 5]
+    assert rotator.rotate(original, 2, 'right') == expected1
+    assert rotator.rotate(original, 1, 'left') == expected2
